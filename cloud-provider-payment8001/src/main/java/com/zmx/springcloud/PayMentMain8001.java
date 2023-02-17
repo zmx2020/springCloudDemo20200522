d08500044eb001c96eb29a33f8585544c0bb967b
zmx2020 <406469415@qq.com>
2023-02-17 21:46:17 +0800

    test1

--- cloud-provider-payment8001/src/main/java/com/zmx/springcloud/PayMentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/zmx/springcloud/PayMentMain8001.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PayMentMain8001 {
+    //zmxtest111
     public static void main(String[] args) {
         SpringApplication.run(PayMentMain8001.class, args);
     }
